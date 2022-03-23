5ed187698a4b3e94197b14b262fd623f6d652439
YusufHass <judekal82@gmail.com>
2022-03-23 17:15:05 -0400

    dynamic coding and adding SASS styling

--- src/App.js
+++ src/App.js
@@ -1,44 +1,41 @@
+import './catagories.styles.scss'
 const App = () => {
+
+  const catagories= [
+
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },{
+      id: 3,
+      title: 'Sneakers'
+    },{
+      id: 4,
+      title: 'Men'
+    },{
+      id: 5,
+      title: 'Women'
+    },
+  ]
   return (
     <div className="catagories-container">
-      <div className="catagory-containeer">
+
+      {catagories.map(({title})=>(
+      <div className="catagory-container">
         {/* {
           imag } */}
+          <p> image</p>
           <div className="catagory-body-container">
-            <h1>Hats</h1>
+            <h1>{title}</h1>
             <p>Shop Now</p>
           </div>
       </div>
-      <div className="catagory-containeer">
-        {/* {
-          imag } */}
-          <div className="catagory-body-container">
-            <h1>Jackets</h1>
-            <p>Shop Now</p>
-          </div>
-      </div><div className="catagory-containeer">
-        {/* {
-          imag } */}
-          <div className="catagory-body-container">
-            <h1>Sneakers </h1>
-            <p>Shop Now</p>
-          </div>
-      </div><div className="catagory-containeer">
-        {/* {
-          imag } */}
-          <div className="catagory-body-container">
-            <h1>Men</h1>
-            <p>Shop Now</p>
-          </div>
-      </div><div className="catagory-containeer">
-        {/* {
-          imag } */}
-          <div className="catagory-body-container">
-            <h1>Women</h1>
-            <p>Shop Now</p>
-          </div>
+      ))}
       </div>
-    </div>
   );
 };
 
