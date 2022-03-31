6a1bc58b90a81d013d99202b853f8747dfdb89dc
YusufHass <judekal82@gmail.com>
2022-03-31 16:41:33 -0400

    Method 2 the way we makes props in the to the component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,17 +1,17 @@
 import "./form-input.styles.scss";
-const FormInput = ({ label, ...otherProps }) => {
+const FormInput = ({ label, inputOptions }) => {
   return (
     <div className="group">
         {/* we added the input above the label because the label is the child of the input
         in the .scss styling  */}
         {/* also the form field is shirnkable when we add text  */}
-      <input className="form-input" {...otherProps} />
+      <input className="form-input" {...inputOptions} />
       {/* if leble exists then render the label */}
       {label && (
         <label
           // if in the input feild of otherProps has input value then shrink otherwise back to default
           className={`${
-            otherProps.value.lengh ? "shrink" : ""
+            inputOptions.value.lengh ? "shrink" : ""
           } form-input-label`}
         >
           {label}
