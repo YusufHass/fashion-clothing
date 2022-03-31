a0b144e450da52656fe98794cb451b048d18638e
YusufHass <judekal82@gmail.com>
2022-03-31 16:29:34 -0400

    shirkable form field

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -4,6 +4,7 @@ const FormInput = ({ label, ...otherProps }) => {
     <div className="group">
         {/* we added the input above the label because the label is the child of the input
         in the .scss styling  */}
+        {/* also the form field is shirnkable when we add text  */}
       <input className="form-input" {...otherProps} />
       {/* if leble exists then render the label */}
       {label && (
