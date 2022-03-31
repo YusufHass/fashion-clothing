03fd320acde745c36c2efc405dd637e7030fbe55
YusufHass <judekal82@gmail.com>
2022-03-31 16:28:01 -0400

    styling the input

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -2,6 +2,8 @@ import "./form-input.styles.scss";
 const FormInput = ({ label, ...otherProps }) => {
   return (
     <div className="group">
+        {/* we added the input above the label because the label is the child of the input
+        in the .scss styling  */}
       <input className="form-input" {...otherProps} />
       {/* if leble exists then render the label */}
       {label && (
