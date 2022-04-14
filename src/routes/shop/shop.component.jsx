96ee7d601a52c4adaf93f9ed7f5fee93803a76b0
YusufHass <judekal82@gmail.com>
2022-04-14 17:22:28 -0400

    Product context is added, the product componet is created

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,14 +1,16 @@
 import SHOP_DATA from "../../shop-data.json";
 import { ProductContext } from "../../contexts/products.context";
 import { useContext } from "react";
+import ProductCard from "../../product-card/product-card.component";
 const Shop = () => {
   const {products}= useContext(ProductContext);
   return (
     <div className="div">
-      {products.map(({name, id}) => (
-        <div key={id}>
-          <h1>{name}</h1>
-        </div>
+      {products.map((product) => (
+        <ProductCard key={product.id} products={product}/>
+        // <div key={id}>
+        //   <h1>{name}</h1>
+        // </div>
       ))}
     </div>
   );
