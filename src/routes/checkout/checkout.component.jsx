93db315ce9f9f785d11afd7b62de3a92c3de372e
YusufHass <judekal82@gmail.com>
2022-05-07 16:29:57 -0400

    added item increment and decrement with the item description list, along with the styles

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,33 +1,47 @@
-import { useContext } from 'react';
-import './checkout.styles.scss'
-import {CartContext} from '../../contexts/cart.context'
+import { useContext } from "react";
+import "./checkout.styles.scss";
+import { CartContext } from "../../contexts/cart.context";
 
 const Checkout = () => {
-    const {cartItems, addItemToCart, removeItemFromCart}= useContext(CartContext)
+  const { cartItems, addItemToCart, removeItemFromCart } =
+    useContext(CartContext);
   return (
-    <div className="div">
-      <h2>Iam the checkout page</h2>
-      <div className="div">
-
-          {
-              cartItems.map((cartItem)=>{
-                  const {id, name ,quantity}= cartItem;
-                  return(
-                  <div className="div" key={id}>
-                      <h1>{name}</h1>
-                      <span>{quantity}</span>
-                      <br/>
-                      <span onClick={()=>addItemToCart(cartItem)}>increment</span>
-                      <br/>
-                      <span onClick={()=>removeItemFromCart(cartItem)}>decrement</span>
-
-
-                  </div>
-                  )
-
-              })
-          }
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
       </div>
+
+      {/* <h2>Iam the checkout page</h2> */}
+        {cartItems.map((cartItem) => {
+          const { id, name, quantity } = cartItem;
+          return (
+            <div className="div" key={id}>
+              <h1>{name}</h1>
+              <span>{quantity}</span>
+              <br />
+              <span onClick={() => addItemToCart(cartItem)}>increment</span>
+              <br />
+              <span onClick={() => removeItemFromCart(cartItem)}>
+                decrement
+              </span>
+            </div>
+          );
+        })}
+        <span className="Total">Total: 0</span>
     </div>
   );
 };
