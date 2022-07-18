e366a013869e4f909ee5886d3b0981de0da2137c
YusufHass <judekal82@gmail.com>
2022-07-18 16:50:08 -0400

    CategoryPreview shows the sample from each category such as Hats, Jackets etc and and then the category

--- src/components/category-preview/category-preview.jsx
+++ src/components/category-preview/category-preview.jsx
@@ -1,16 +1,17 @@
 // this component creats the category for each title.
 //example the first page diplays the 'hat' with 4 products and when it cl
 // clicked then it diplays the rest of 'hats' similar products
-
-import { keyboard } from "@testing-library/user-event/dist/keyboard";
+import { Link } from "react-router-dom";
 import ProductCard from "../../product-card/product-card.component";
 import "./category-preview.styles.scss";
 const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
       <h2>
-        {/* the following span makes the the title makes a link only on the title */}
-        <span className="title">{title.toUpperCase()}</span>
+        {/* the following Link makes the the title a link and when we c
+        click the title such as hats and jackets directs us to the another page of the page that shows mo
+  more items of similar  category */}
+        <Link className="category-preview-title" to={title}>{title.toUpperCase()}</Link>
       </h2>
       <div className="preview">
       {/* // if the index is such as the first item index 0, second index 1 etc.. or the items are less than 4 then display them
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,39 +1,39 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { useEffect } from "react";
 import { useContext } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { CategoriesContext } from "../../contexts/categories.context";
 import ProductCard from "../../product-card/product-card.component";
 
-import './category.styles.scss'
+import "./category.styles.scss";
 
-const Category=()=>{
-//useParms used to generate the path 'category' in the shops.component
-const {category}= useParams();
-const {categoriesMap}=useContext(CategoriesContext);
-//since categoriesMap use async data generation the we have to 
-//wait untile the async data properly generated and populated to products. Otherwise the product would be empty since it runs sync and would couse error on the program
-const [products, setProducts]=useState(categoriesMap[category]);
+const Category = () => {
+  //useParms used to generate the path 'category' in the shops.component
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  //since categoriesMap use async data generation the we have to
+  //wait untile the async data properly generated and populated to products. Otherwise the product would be empty since it runs sync and would couse error on the program
+  const [products, setProducts] = useState(categoriesMap[category]);
 
-useEffect(()=>{
-
+  useEffect(() => {
     setProducts(categoriesMap[category]);
-// unless the category and categoriesMap have changed, the useEffect will not updated
-}, [category, categoriesMap])
-return (
-
-    <div  className="category-container">
+    // unless the category and categoriesMap have changed, the useEffect will not updated
+  }, [category, categoriesMap]);
+  return (
+    <Fragment>
+      <h1 className="category-title">{category.toUpperCase()}</h1>
+      <div className="category-container">
         {
-            //the following && makes the safe guird for the program work
-            // it says unless the products has value and defined then dont diplay anything/
-          products &&  products.map((product)=>(
-                <ProductCard key={product.id} products={product}/>
-
-            )
-            )
+          //the following && makes the safe guird for the program work
+          // it says unless the products has value and defined then dont diplay anything/
+          products &&
+            products.map((product) => (
+              <ProductCard key={product.id} products={product} />
+            ))
         }
-    </div>
-)
+      </div>
+    </Fragment>
+  );
 };
 
 export default Category;
